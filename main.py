5b14a0e3ecafacffd357bafb054cd59b1545d186
Carnu Stefan <crn.stefan@gmail.com>
2021-10-13 11:39:05 +0300

    Done

--- main.py
+++ main.py
@@ -2,32 +2,57 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
-  
+  if (n<=2): return False
+  for i in range(2,n//2):
+      if(n%i==0):return False
+  return True
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
-  
+  produs=1
+  for i in range (len(lst)):produs=produs*lst[i]
+  return produs
+
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
+  while(x!=y):
+    if (x>y):x=x-y
+    if (x<y):y=y-x
+  return x
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  while(y!=0):
+    r=x%y
+    x=y
+    y=r
+  return x
+
   
   
 def main():
-  # interfata de tip consola aici
+    n=int(input('Introduceti n:'))
+    nr=int(input('Introduceti nr de numere din lista:'))
+    lst=[]
+    for i in range (nr):
+      lst.append(int(input()))
+    
+    x=int(input('Introduceti x:'))
+    y=int(input('Introduceti y:'))
+    print (is_prime(n))
+
+    print (get_product(lst))
+
+    print (get_cmmdc_v1(x, y))
+
+    print (get_cmmdc_v2(x, y))
+
 
 if __name__ == '__main__':
   main()
